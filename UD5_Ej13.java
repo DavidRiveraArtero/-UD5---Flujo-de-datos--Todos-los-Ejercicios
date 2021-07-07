54a25ce22e35e95aed47c87ed9b738a952aa1cf6
DavidRiveraArtero <87012638+DavidRiveraArtero@users.noreply.github.com>
2021-07-07 14:31:40 +0200

    Update UD5_Ej13.java

--- UD5_Ej13.java
+++ UD5_Ej13.java
@@ -3,13 +3,14 @@ import javax.swing.JOptionPane;
 public class UD5_Ej13 {
 
 	public static void main(String[] args) {
-		
+		//LE PEDIMOS AL USUARIO QUE NOS DE EL PRIMER NUMERO
 		String num1 = JOptionPane.showInputDialog("DAME EL PRIMER NUMERO: ");
 		
+		//LUEGO LO PASAMOS A INT
 		Double numero1 = Double.parseDouble(num1);
-		
+		//LE PEDIMOS AL USUARIO QUE NOS DE EL PRIMER NUMERO
 		String num2 = JOptionPane.showInputDialog("DAME EL SEGUNDO NUMERO: ");
-		
+		//LUEGO LO PASAMOS A INT
 		Double numero2 = Double.parseDouble(num2);
 		
 		String operacion = JOptionPane.showInputDialog("QUE OPERACION QUIERES HACER: + , - , * , / , ^ , %");
